9747b48fc4778469136dfad0bed543e27770618d
Vitaly Bibik <wital2010@gmail.com>
2021-04-25 01:47:31 +0300

    test

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import test from "@ya.praktikum/react-developer-burger-ui-components";
+import {BurgerIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
 export const BurgerIngredients = () => {
-    console.log(test)
-    return <h1> www</h1>
+    return 
 }
---
+++ src/components/ShowLibrary/ShowLibrary.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    BurgerIcon,
+    CheckMarkIcon, CloseIcon,
+    CurrencyIcon, DragIcon,
+    EditIcon,
+    HideIcon, InfoIcon, ListIcon, LockIcon,
+    LogoutIcon, ProfileIcon,
+    ShowIcon
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+export const ShowLibrary = () => {
+    <div>
+        <CurrencyIcon type="primary" />
+    <BurgerIcon type="primary" />
+    <LockIcon type="primary" />
+    <DragIcon type="primary" />
+    <CloseIcon type="primary" />
+    <CheckMarkIcon type="primary" />
+    <ListIcon type="primary" />
+    <ProfileIcon type="primary" />
+    <EditIcon type="primary" />
+    <InfoIcon type="primary" />
+    <ShowIcon type="primary" />
+    <HideIcon type="primary" />
+    <LogoutIcon type="primary" />
+        </div>
+}
