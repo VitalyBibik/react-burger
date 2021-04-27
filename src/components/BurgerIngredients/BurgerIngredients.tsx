a7eda056de63b451a3978830e34a7d973f31a96f
Vitaly Bibik <wital2010@gmail.com>
2021-04-27 13:16:43 +0300

    add tabs

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -1,11 +1,25 @@
 import React, { memo } from 'react'
-import {  } from "@ya.praktikum/react-developer-burger-ui-components";
+import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import style from './BurgerIngredients.module.scss'
+import cn from 'classnames'
 
 export const BurgerIngredients = memo(() => {
+  const [current, setCurrent] = React.useState('Булки')
     return (
       <div className={style.container}>
-          <h1>Соберите Бургер</h1>
+          <h1 className={cn("text text_type_main-large", style['container__title'])}>Соберите Бургер</h1>
+        <div style={{ display: 'flex' }}>
+          <Tab value="Булки" active={current === 'Булки'} onClick={setCurrent}>
+            Булки
+          </Tab>
+          <Tab value="Соусы" active={current === 'Соусы'} onClick={setCurrent}>
+            Соусы
+          </Tab>
+          <Tab value="Начинки" active={current === 'Начинки'} onClick={setCurrent}>
+            Начинки
+          </Tab>
+        </div>
       </div>
+
     )
 })
