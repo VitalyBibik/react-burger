2e3260af7996365b6935ee0f5a9247d2c1b95108
Vitaly Bibik <wital2010@gmail.com>
2021-04-28 15:00:16 +0300

    card is almost done

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -2,9 +2,36 @@ import React, { memo } from 'react'
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components'
 import style from './BurgerIngredients.module.scss'
 import cn from 'classnames'
+import { data } from '../../fixtures'
+import { BurgerItem } from '../BurgerItem'
 
 export const BurgerIngredients = memo(() => {
   const [current, setCurrent] = React.useState('Булки')
+  const [state, setState] = React.useState({
+    isLoading: false,
+    hasError: false,
+    data: []
+  })
+  // React.useEffect(() => {
+  //   getProducts();
+  // }, [])
+  //
+  // const getProducts = async () => {
+  //   setState({ ...state, hasError: false, isLoading: true });
+  //   try {
+  //     const res = await fetch('https://api.nomoreparties.co/')
+  //     const data = await res.json()
+  //     setState({ ...state, data, isLoading: false })
+  //   }
+  //   catch {
+  //     setState({ ...state, hasError: true, isLoading: false });
+  //   }
+  // }
+
+  const sauceArray = data.filter((el) => el.type == 'sauce')
+  const fillingArray = data.filter((el) => el.type == 'main')
+  const breadArray = data.filter((el) => el.type == 'bun')
+
     return (
       <div className={style.container}>
           <h1 className={cn("text text_type_main-large", style['container__title'])}>Соберите Бургер</h1>
@@ -19,6 +46,18 @@ export const BurgerIngredients = memo(() => {
             Начинки
           </Tab>
         </div>
+        <h2 className={cn("text text_type_main-large", style['container__title'])}>Булки</h2>
+          <div className={style.grid}>
+            {breadArray.map((el) => <BurgerItem key={el._id} {...el} /> )}
+          </div>
+        <h2 className={cn("text text_type_main-large", style['container__title'])}>Соусы</h2>
+          <div className={style.grid}>
+            {sauceArray.map((el) => <BurgerItem key={el._id} {...el} /> )}
+          </div>
+        <h2 className={cn("text text_type_main-large", style['container__title'])}>Начинки</h2>
+          <div className={style.grid}>
+            {fillingArray.map((el) => <BurgerItem key={el._id} {...el} /> )}
+          </div>
       </div>
     )
 })
---
+++ src/components/BurgerItem/BurgerItem.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+import style from './BurgerItem.module.scss'
+import cn from 'classnames'
+
+interface BurgerItem {
+  "_id": string,
+  "name":string,
+  "type":string,
+  "proteins":number,
+  "fat":number,
+  "carbohydrates":number,
+  "calories":number,
+  "price":number,
+  "image":string,
+  "image_mobile":string,
+  "image_large":string,
+  "__v":number
+}
+
+export const BurgerItem = (props:BurgerItem) => {
+  console.log(props.calories)
+
+  return (
+    <div className={style.container}>
+      <div className={style['container__image']}>
+        <img className={style.image} srcSet={props.image_large} alt={'text'}/>
+        <Counter count={1} size='small' />
+      </div>
+      <div className={style['container__price']}>
+        <span className={cn("text text_type_digits-default", style['container__position'])}>{props.price}</span>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className={cn('text text_type_main-default', style['container__description'])}>{props.name}</p>
+    </div>)
+}
