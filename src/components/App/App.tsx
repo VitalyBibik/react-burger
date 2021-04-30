f88e6bae3f6b48bbcf4e16df3a7e371fe31ca63c
Vitaly Bibik <wital2010@gmail.com>
2021-04-30 11:36:45 +0300

    fix issues 4

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './App.module.scss';
+import { AppHeader } from "../AppHeader";
+import { BurgerUnion } from '../BurgerUnion'
+
+export function App() {
+  return (
+    <div className={style.App}>
+        <AppHeader />
+        <BurgerUnion />
+    </div>
+  );
+}
+
+
