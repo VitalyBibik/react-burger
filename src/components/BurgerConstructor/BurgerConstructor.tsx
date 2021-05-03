04f24b9782e8eb0da41c904163b82a3a6e71466a
Vitaly Bibik <wital2010@gmail.com>
2021-05-04 01:50:43 +0300

    ÐStart Popup

--- src/components/BurgerConstructor/BurgerConstructor.tsx
+++ src/components/BurgerConstructor/BurgerConstructor.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import style from './BurgerConstructor.module.scss';
 import { OrderItem } from '../OrderItem';
 import { PriceItem } from '../PriceItem';
+import { Popup } from '../Popup';
+import { useDisclosure } from '../../hooks';
 
 export const BurgerConstructor = ({ bread, productArray }: any) => {
+
+  const { isOpen, toggle } = useDisclosure(false, {
+    onOpen: () => console.log("я открылся")
+  });
+
   return (
+    <>
     <div className={style.container}>
       <OrderItem bread={bread} top />
       <ul className={style.container__item}>
@@ -14,10 +22,13 @@ export const BurgerConstructor = ({ bread, productArray }: any) => {
       <OrderItem bread={bread} top={false} />
       <div className={style.container__button}>
         <PriceItem price={610} size="medium" />
-        <Button type="primary" size="medium">
+        <Button type="primary" size="medium" onClick={toggle}>
           Оформить заказ
         </Button>
       </div>
+      {isOpen && <Popup open={isOpen} />}
     </div>
+
+    </>
   );
 };
