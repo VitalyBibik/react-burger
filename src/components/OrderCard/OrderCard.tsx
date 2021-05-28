ebf8544ffc7f81c6716c8cf4f85c3dadd376d241
Vitaly Bibik <wital2010@gmail.com>
2021-05-28 13:44:49 +0300

    start to do 2step

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react'
 import {
   ConstructorElement,
   DragIcon
@@ -6,6 +6,7 @@ import {
 import style from './OrderCard.module.scss';
 import { useDispatch } from 'react-redux';
 import { remove } from '../../services/ducks/constructor';
+import { useDrag, useDrop } from 'react-dnd';
 
 type OrderItemIngredient = {
   _id: string;
@@ -29,11 +30,68 @@ type PropsOrderCard = {
 
 export const OrderCard = memo(({ card }: PropsOrderCard) => {
   const dispatch = useDispatch()
+  const ref = useRef<HTMLDListElement>(null )
   const handleClose = () => {
     dispatch(remove(card))
   }
+  const [{ handlerId }, drop] = useDrop({
+    accept: ItemTypes.CARD,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      // Get vertical middle
+      const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.CARD,
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref))
   return (
-          <li className={style.container}>
+          <li className={style.container} ref={ref}>
             <div className={style['container__icon']}><DragIcon type="primary" /></div>
             <ConstructorElement
               text={card.name}
