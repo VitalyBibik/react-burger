52d9ca68a46648b8f9fbbc2a20670d5ae3546abe
Vitaly Bibik <wital2010@gmail.com>
2021-06-07 14:39:00 +0300

    add render DetailCard

--- src/components/OrderHistoryDetailCard/OrderHistoryDetailCard.tsx
+++ src/components/OrderHistoryDetailCard/OrderHistoryDetailCard.tsx
@@ -29,30 +29,28 @@ type OrderItemIngredient = {
 
 export const OrderHistoryDetailCard = memo(() => {
     const { id }  = useParams<any>();
-    console.log(id)
-    const dispatch = useDispatch()
-    useEffect(() => {
-        dispatch(loadIngredients())
-    }, [dispatch]);
-  const bunItem = useSelector((store:any) => store.constructorReducer.data.filter((el:any) => el.type === BUN))
+  //   const dispatch = useDispatch()
+  //   useEffect(() => {
+  //       dispatch(loadIngredients())
+  //   }, [dispatch]);
+  // const bunItem = useSelector((store:any) => store.constructorReducer.data.filter((el:any) => el.type === BUN))
   const colors = {
     canceled:style.canceled,
     inProgress:style.inProgress,
     isDone:style.isDone,
   }
-  const testArray = useMemo(() => [data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[11], data[11]], []);
-    const arrayCount = useMemo(() => {
-        const counters:any = {};
-        testArray.forEach((ingredient:any) => {
-            counters[ingredient._id] = data.filter(
-                (item:any) => item._id === ingredient._id
-            ).length;
-            if (bunItem && bunItem._id === ingredient._id) {
-                counters[ingredient._id] += 2;
-            }
-        })
-        return counters;
-    },[bunItem, testArray])
+  const testArray = useMemo(() => [data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[11], data[11]], []);
+  const ingredients = useMemo(() => Object.values(testArray.reduce((acc:any, item:any) => {
+    if (!acc[item._id]) {
+      acc[item._id] = {
+        count: 0,
+        data: item,
+      };
+    }
+    acc[item._id].count++;
+    return acc;
+  }, {})),[testArray])
+  console.log(ingredients)
 
   const sum = useMemo(() => getSum(testArray), [testArray]);
   return (
@@ -70,56 +68,18 @@ export const OrderHistoryDetailCard = memo(() => {
               <h3 className={cn('text text_type_main-medium')}>Cостав:</h3>
           </div>
         <ul className={style.container}>
-             <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
-            <li className={cn(style.card, 'mr-4')}>
-                <img src={testArray[0].image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
-                <p className={cn('text text_type_main-default', style.right)}>Флюоресцентная булка R2-D3</p>
-                <span className={cn('text text_type_digits-default', style.numbers)}>2&thinsp;x&thinsp;<PriceItem price={20} /></span>
-            </li>
+          { ingredients.map((el:any) => {
+            return (
+              <li className={cn(style.card, 'mr-4')} key={el.data._id}>
+                <img src={el.data.image_mobile} alt="eda" className={cn(style.img, 'mr-4')}/>
+                <p className={cn('text text_type_main-default', style.right)}>{el.data.name}</p>
+                <span className={cn('text text_type_digits-default', style.numbers)}>{el.count}&thinsp;x&thinsp;<PriceItem price={el.data.price} /></span>
+              </li>
+            )
+          })
+
+          }
+
         </ul>
         <div className={style.container_mini}>
             <p className={cn('text text_type_main-default text_color_inactive')}>Сегодня, 16:20 i-GMT+3</p>
