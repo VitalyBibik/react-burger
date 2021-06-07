406e64f7b553fdd1e9a52d78c04275a576e9e5ac
Vitaly Bibik <wital2010@gmail.com>
2021-06-08 02:17:10 +0300

    small fix

--- src/components/OrdersHistoryCard/OrderHistoryCard.tsx
+++ src/components/OrdersHistoryCard/OrderHistoryCard.tsx
@@ -23,14 +23,13 @@ type OrderHistoryCardProps = {
   length?:number,
   index:number
 }
-
 export const OrderHistoryCard = memo(({ card, last = false, length, index } : OrderHistoryCardProps ) => {
-
+// TODO Сделать индексы в обратом порядке
   return (
     <>
       {
         !last ? <li className={style.list}><img src={card.image_mobile} alt={card.name} className={style.img} style={{zIndex:index}} /></li>
-             : <li className={style.list}><img src={card.image_mobile} alt={card.name} className={cn(style.img, style.img_last)} style={{zIndex:index}} />
+             : <li className={style.list}><img src={card.image_mobile} alt={card.name} className={cn(style.img, style.img_last)} />
                 <span className={cn(style.number, 'text text_type_main-default')}>{`+${length}`}</span>
                </li>
        }
