162f2fc54de1c113bd1af8519ac457a9752596c8
Vitaly Bibik <wital2010@gmail.com>
2021-06-16 02:20:06 +0300

    fix ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -30,7 +30,7 @@ export function App() {
   const emailWasSent = useSelector(getIsEmailSent);
 
   const history = useHistory();
-  let location = useLocation();
+  let location = useLocation<any>();
 
   let background =
     (history.action === 'PUSH' || history.action === 'REPLACE') &&
