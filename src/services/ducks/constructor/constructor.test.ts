6f86c9047920fd02815ebd7116bb672ab0028b56
Vitaly Bibik <wital2010@gmail.com>
2021-06-21 01:12:11 +0300

    Test is failed

--- src/services/ducks/constructor/constructor.test.ts
+++ src/services/ducks/constructor/constructor.test.ts
@@ -9,10 +9,6 @@ import {
 } from './slice';
 import { data } from '../../../fixtures';
 import { v4 as uuid } from 'uuid';
-import configureMockStore from 'redux-mock-store'
-import fetchMock from 'fetch-mock'
-import {types} from "util";
-import thunk from "redux-thunk";
 
 const initialState: burgerState = {
   isLoading: false,
@@ -135,32 +131,3 @@ describe(`${sliceName} Reducers`, () => {
     );
   });
 });
-
-
-
-const middlewares = [thunk]
-const mockStore = configureMockStore(middlewares)
-
-describe('async actions', () => {
-    afterEach(() => {
-        fetchMock.restore()
-    })
-
-    it('creates FETCH_TODOS_SUCCESS when fetching todos has been done', () => {
-        fetchMock.getOnce('/todos', {
-            body: { todos: ['do something'] },
-            headers: { 'content-type': 'application/json' }
-        })
-
-        const expectedActions = [
-            { type: types.FETCH_TODOS_REQUEST },
-            { type: types.FETCH_TODOS_SUCCESS, body: { todos: ['do something'] } }
-        ]
-        const store = mockStore({ todos: [] })
-
-        return store.dispatch(actions.fetchTodos()).then(() => {
-            // Возвращаем асинхронный экшен
-            expect(store.getActions()).toEqual(expectedActions)
-        })
-    })
-})
