f3706ec4da0aa2a7a0371efb59aca6b7bb8e41c1
Vitaly Bibik <61142441+VitalyBibik@users.noreply.github.com>
2021-07-14 14:01:07 +0300

    Step-3(final) (#16)
    * 3 step
    
    * fix path

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -16,7 +16,6 @@ export const ProtectedRoute: FC<RouteProps> = ({ children, ...rest }) => {
   const hasToken = !!getRefreshToken()
 
   useEffect(() => {
-    console.log(hasToken && !isTokenUpdated && !tokenUpdating, 'tokenProtect')
     if (hasToken && !isTokenUpdated && !tokenUpdating) {
       dispatch(refreshToken(null))
     }
